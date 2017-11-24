4e9d1862858861d20e515bf28f8a16a90e737d38
212414 <212414@etud.uco.fr>
2017-11-24 18:05:12 +0100

    abstract dao + structure de DAO chambre

---
+++ uco.ima.bdd.projet2017/src/DAO/chambreDAO.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import model.chambre;
+
+public class chambreDAO extends DAO<chambre>{
+
+	@Override
+	public boolean create(chambre obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(chambre obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(chambre obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public chambre find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
