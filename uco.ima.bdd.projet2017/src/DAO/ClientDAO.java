8362816492c44945d9b2ad02602acd40be4a2846
212553 <212553@etud.uco.fr>
2017-11-28 11:59:32 +0100

    Ajout ClientDAO dans package DAO

---
+++ uco.ima.bdd.projet2017/src/DAO/ClientDAO.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import model.Client;
+
+public class ClientDAO extends DAO<Client>{
+
+	@Override
+	public boolean create(Client obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Client obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Client obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Client find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
