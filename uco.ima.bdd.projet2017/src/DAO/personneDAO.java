d435b78be13c935da61655b5e6f04904f21e7bba
212414 <212414@C018-06.uco.local>
2017-11-29 12:09:11 +0100

    DAO personne

--- uco.ima.bdd.projet2017/src/DAO/personneDAO.java
+++ uco.ima.bdd.projet2017/src/DAO/personneDAO.java
@@ -2,26 +2,40 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import Singleton.SingletonConnection;
-import model.personne;
+import model.Personne;
 
-public class personneDAO extends DAO<personne> {
+public class personneDAO extends DAO<Personne> {
 
 	Connection SC = SingletonConnection.getConnection();
 	
+	/*
+	private int id_personne;
+	private String nom;
+	private String prenom;
+	private String ville;
+	private int code_postal;
+	private String adresse;
+	private Date date_de_naissance;
+	 */
+	
 	@Override
-	public boolean create(personne obj) {
+	public boolean create(Personne obj) {
 		try {
 
 			PreparedStatement prepare = SC
-					.prepareStatement("Insert into abonne values (?,?,?,?);");
+					.prepareStatement("Insert into personne values (?,?,?,?,?,?,?);");
 
-			prepare.setInt(1, obj.getIdabonne());
+			prepare.setInt(1, obj.getId_personne());
 			prepare.setString(2, obj.getNom());
 			prepare.setString(3, obj.getPrenom());
-			prepare.setInt(4, obj.getNbemprunt());
+			prepare.setString(4, obj.getVille());
+			prepare.setInt(5, obj.getCode_postal());
+			prepare.setString(6, obj.getAdresse());
+			prepare.setDate(7, obj.getDate_de_naissance());
 
 			prepare.executeUpdate();
 
@@ -33,21 +47,64 @@ public class personneDAO extends DAO<personne> {
 	}
 
 	@Override
-	public boolean delete(personne obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Personne obj) {
+		
+		try{
+			PreparedStatement prepare =SC.prepareStatement("Delete from personne where id_personne=?");
+
+			prepare.setInt(1, obj.getId_personne());
+			
+			prepare.executeUpdate();
+			
+			return true;
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
-	public boolean update(personne obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Personne obj) {
+		try{
+			PreparedStatement prepare=SC.prepareStatement("Update personne set nom=?, prenom=?, ville=?, code_postal=?, adresse=?, date_naissance=? where id_personne=?");
+			
+			prepare.setString(1, obj.getNom());
+			prepare.setString(2, obj.getPrenom());
+			prepare.setString(3, obj.getVille());
+			prepare.setInt(4, obj.getCode_postal());
+			prepare.setString(5, obj.getAdresse());
+			prepare.setDate(6, obj.getDate_de_naissance());
+			
+			prepare.executeUpdate();
+			return true;	
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
-	public personne find(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Personne find(int id) {
+		Personne personne = new Personne();
+		try {
+			PreparedStatement prepare = SC.prepareStatement("SELECT * FROM personne where id_personne = ?");
+			prepare.setInt(1, id);
+			ResultSet result = prepare.executeQuery();
+			
+			if(result.first()){
+				personne.setId_personne(result.getInt("id_personne"));
+				personne.setNom(result.getString("nom"));
+				personne.setPrenom(result.getString("prenom"));
+				personne.setVille(result.getString("ville"));
+				personne.setCode_postal(result.getInt("code_postal"));
+				personne.setAdresse(result.getString("adresse"));
+				personne.setDate_de_naissance(result.getDate("date_naissance"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return personne;
 	}
 
 }
